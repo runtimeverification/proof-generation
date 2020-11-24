de3f087002afcf4d472f1253279ee5bc12b939b9
Xiaohong <xc3@illinois.edu>
2020-11-24 15:53:02 -0600

    matching-logic.mm: revise upto #Equal

--- theory/matching-logic.mm
+++ theory/matching-logic.mm
@@ -1,53 +1,201 @@
-$(
-    Functional matching logic
-$)
+$( MATCHING LOGIC $)
 
 $c #Pattern $.
 $c #ElementVariable $.
 $c #SetVariable $.
 $c #Variable $.
-$c ( ) $.
+$c #Symbol $.
 
 $v ph0 ph1 ph2 ph3 ph4 ph5 ph6 ph7 ph8 $.
 $v x y z w $.
 $v X Y Z W $.
 $v xX yY $.
+$v sg0 sg1 sg2 sg3 sg4 $.
 
-ph0-pattern $f #Pattern ph0 $.
-ph1-pattern $f #Pattern ph1 $.
-ph2-pattern $f #Pattern ph2 $.
-ph3-pattern $f #Pattern ph3 $.
-ph4-pattern $f #Pattern ph4 $.
-ph5-pattern $f #Pattern ph5 $.
-ph6-pattern $f #Pattern ph6 $.
-ph7-pattern $f #Pattern ph7 $.
-ph8-pattern $f #Pattern ph8 $.
+ph0-is-pattern $f #Pattern ph0 $.
+ph1-is-pattern $f #Pattern ph1 $.
+ph2-is-pattern $f #Pattern ph2 $.
+ph3-is-pattern $f #Pattern ph3 $.
+ph4-is-pattern $f #Pattern ph4 $.
+ph5-is-pattern $f #Pattern ph5 $.
+ph6-is-pattern $f #Pattern ph6 $.
+ph7-is-pattern $f #Pattern ph7 $.
+ph8-is-pattern $f #Pattern ph8 $.
 
-x-element-var $f #ElementVariable x $.
-y-element-var $f #ElementVariable y $.
-z-element-var $f #ElementVariable z $.
-w-element-var $f #ElementVariable w $.
+x-is-element-var $f #ElementVariable x $.
+y-is-element-var $f #ElementVariable y $.
+z-is-element-var $f #ElementVariable z $.
+w-is-element-var $f #ElementVariable w $.
 
-X-element-var $f #SetVariable X $.
-Y-element-var $f #SetVariable Y $.
-Z-element-var $f #SetVariable Z $.
-W-element-var $f #SetVariable W $.
+X-is-element-var $f #SetVariable X $.
+Y-is-element-var $f #SetVariable Y $.
+Z-is-element-var $f #SetVariable Z $.
+W-is-element-var $f #SetVariable W $.
 
-xX-var $f #Variable xX $.
-yY-var $f #Variable yY $.
+xX-is-var $f #Variable xX $.
+yY-is-var $f #Variable yY $.
+
+sg0-is-symbol $f #Symbol sg0 $.
+sg1-is-symbol $f #Symbol sg1 $.
+sg2-is-symbol $f #Symbol sg2 $.
+sg3-is-symbol $f #Symbol sg3 $.
+sg4-is-symbol $f #Symbol sg4 $.
 
 element-var-is-var $a #Variable x $.
-set-var-is-var $a #Variable X $.
-var-is-pattern $a #Pattern xX $.
+set-var-is-var     $a #Variable X $.
+var-is-pattern     $a #Pattern xX $.
 
-$( Substitution $)
+$c #Positive $.
+$c #Negative $.
+$c #Fresh $.
+$c #ApplicationContext $.
 $c #Substitution $.
+$c #Equals $.
+$c |- $.
 
-substitution-var $a #Substitution ph0 xX ph0 xX $.
-${
-    $d xX yY $.
-    substitution-distinct-var $a #Substitution yY yY ph0 xX $.
+$c \bot    $.
+$c \imp    $.
+$c \app    $.
+$c \exists $.
+$c \mu     $.
+$c ( )     $.
+
+bot-is-pattern $a #Pattern \bot $.
+imp-is-pattern $a #Pattern ( \imp ph0 ph1 ) $.
+app-is-pattern $a #Pattern ( \app ph0 ph1 ) $.
+exists-is-pattern $a #Pattern ( \exists x ph0 ) $.
+${ 
+    mu-is-pattern.0 $e #Positive X ph0 $.
+    mu-is-pattern   $a #Pattern ( \mu X ph0 ) $.
 $}
 
+$( #Positive and #Negative $)
+
+positive-in-var $a #Positive xX yY $.
+positive-in-symbol $a #Positive xX sg0 $.
+positive-in-bot $a #Positive xX \bot $.
+${
+    positive-in-imp.0 $e #Negative xX ph0 $.
+    positive-in-imp.1 $e #Positive xX ph1 $.
+    positive-in-imp   $a #Positive xX ( \imp ph0 ph1 ) $.
+$}
+${
+    positive-in-app.0 $e #Positive xX ph0 $.
+    positive-in-app.1 $e #Positive xX ph1 $.
+    positive-in-app   $a #Positive xX ( \app ph0 ph1 ) $.
+$}
+${
+    positive-in-exists.0 $e #Positive xX ph0 $.
+    positive-in-exists   $a #Positive xX ( \exists x ph0 ) $.
+$}
+${
+    positive-in-mu.0 $e #Positive xX ph0 $.
+    positive-in-mu   $a #Positive xX ( \mu X ph0 ) $.
+$}
+
+${
+    $d xX yY $.
+    negative-in-var $a #Negative xX yY $.
+$}
+${
+    negative-in-imp.0 $e #Positive xX ph0 $.
+    negative-in-imp.1 $e #Negative xX ph1 $.
+    negative-in-imp   $a #Negative xX ( \imp ph0 ph1 ) $.
+$}
+${
+    negative-in-app.0 $e #Negative xX ph0 $.
+    negative-in-app.1 $e #Negative xX ph1 $.
+    negative-in-app   $a #Negative xX ( \app ph0 ph1 ) $.
+$}
+${
+    negative-in-exists.0 $e #Negative xX ph0 $.
+    negative-in-exists   $a #Negative xX ( \exists x ph0 ) $.
+$}
+${
+    negative-in-mu.0 $e #Negative xX ph0 $.
+    negative-in-mu   $a #Negative xX ( \mu X ph0 ) $.
+$}
+
+$( Fresh $)
+
+${
+    $d xX yY $.
+    fresh-in-var $a #Fresh xX yY $.
+$}
+fresh-in-symbol $a #Fresh xX sg0 $.
+fresh-in-bot    $a #Fresh xX \bot $.
+${
+    fresh-in-imp.0 $e #Fresh xX ph0 $.
+    fresh-in-imp.1 $e #Fresh xX ph1 $.
+    fresh-in-imp   $a #Fresh xX ( \imp ph0 ph1 ) $.
+$}
+${
+    fresh-in-app.0 $e #Fresh xX ph0 $.
+    fresh-in-app.1 $e #Fresh xX ph1 $.
+    fresh-in-app   $a #Fresh xX ( \app ph0 ph1 ) $.
+$}
+${
+    $d xX x $.
+    fresh-in-exists $a #Fresh xX ( \exists x ph0 ) $.
+$}
+fresh-in-exists-shadowed $a #Fresh x ( \exists x ph0 ) $.
+${
+    $d xX X $.
+    fresh-in-mu $a #Fresh xX ( \mu X ph0 ) $.
+$}
+fresh-in-mu-shadowed $a #Fresh X ( \mu X ph0 ) $.
+
+$( Substitution $)
+$( #Substitution ph0 ph1 ph2 Xx means ph0 === ph1[ph2/xX] $)
+
+substitution-var-same $a #Substitution ph0 xX ph0 xX $.
+${
+    $d xX yY $.
+    substitution-var-diff $a #Substitution yY yY ph0 xX $.
+$}
+substitution-symbol $a #Substitution sg0 sg0 ph0 xX $.
+substitution-bot    $a #Substitution \bot \bot ph0 xX $.
+${
+    substitution-imp.0  $e #Substitution ph1 ph3 ph0 xX $.
+    substitution-imp.1  $e #Substitution ph2 ph4 ph0 xX $.
+    substitution-imp    $a #Substitution ( \imp ph1 ph2 ) ( \imp ph3 ph4 ) ph0 xX $.
+$}
+${
+    substitution-app.0  $e #Substitution ph1 ph3 ph0 xX $.
+    substitution-app.1  $e #Substitution ph2 ph4 ph0 xX $.
+    substitution-app    $a #Substitution ( \app ph1 ph2 ) ( \app ph3 ph4 ) ph0 xX $.
+$}
+substitution-exists-shadowed $a #Substitution ( \exists x ph1 ) ( \exists x ph1 ) ph0 x $.
+${
+    $d xX x  $.
+    $d y ph0 $.
+    substitution-exists.0 $e #Substitution ph2 ph1 y x $.
+    substitution-exists.1 $e #Substitution ph3 ph2 ph0 xX $.
+    substitution-exists   $a #Substitution ( \exists y ph3 ) ( \exists x ph1 ) ph0 xX $.
+$}
+substitution-mu-shadowed $a #Substitution ( \mu X ph1 ) ( \mu X ph1 ) ph0 X $.
+${
+    $d xX X  $.
+    $d Y ph0 $.
+    substitution-mu.0 $e #Substitution ph2 ph1 Y X $.
+    substitution-mu.1 $e #Substitution ph3 ph2 ph0 xX $.
+    substitution-mu   $a #Substitution ( \mu Y ph3 ) ( \mu X ph1 ) ph0 xX $.
+$}
+
+$( Application Contexts $)
+
+application-context-var $a #ApplicationContext xX xX $.
+${
+    $d xX ph1 $.
+    application-context-app-left.0 $e #ApplicationContext xX ph0 $.
+    application-context-app-left   $a #ApplicationContext xX ( \app ph0 ph1 ) $.
+$}
+${
+    $d xX ph0 $.
+    application-context-app-right.0 $e #ApplicationContext xX ph1 $.
+    application-context-app-right   $a #ApplicationContext xX ( \app ph0 ph1 ) $.
+$}
+
+$c #Equals $.
 $( Provability $)
 $c |- $.
