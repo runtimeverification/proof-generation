b4a8b338c57b3043e930d7af3beb46c1a27bcb6a
rod-lin <zl38@illinois.edu>
2020-12-26 23:06:26 -0600

    add a proof for a slightly weaker version of functional substitution

--- theory/matching-logic.mm
+++ theory/matching-logic.mm
@@ -150,6 +150,11 @@ ${
     $d xX ph0 $.
     fresh-disjoint $a #Fresh xX ph0 $.
 $}
+${
+    fresh-in-substitution.0 $e #Fresh xX ph1 $.
+    fresh-in-substitution.1 $e #Substitution ph2 ph0 ph1 xX $.
+    fresh-in-substitution $a #Fresh xX ph2 $.
+$}
 
 $( Substitution $)
 $( #Substitution ph0 ph1 ph2 xX means ph0 === ph1[ph2/xX] $)
@@ -187,8 +192,16 @@ ${
     substitution-mu.1 $e #Substitution ph3 ph2 ph0 xX $.
     substitution-mu   $a #Substitution ( \mu Y ph3 ) ( \mu X ph1 ) ph0 xX $.
 $}
-
 substitution-identity $a #Substitution ph0 ph0 xX xX $.
+${
+    substitution-inverse.0 $e #Fresh xX ph0 $.
+    substitution-inverse.1 $e #Substitution ph1 ph0 xX yY $.
+    substitution-inverse   $a #Substitution ph0 ph1 yY xX $.
+$}
+${
+    substitution-fresh.0 $e #Fresh xX ph0 $.
+    substitution-fresh $a #Substitution ph0 ph0 ph1 xX $.
+$}
 
 $( Application Contexts $)
 
