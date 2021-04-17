2e55a64c3cc1f5c886b358bd096f37c4d10abe97
Nishant Rodrigues <nishantjr@gmail.com>
2021-04-17 07:00:14 -0500

    Simplified implimentation of is_satisfiable

--- tableau.py
+++ tableau.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 from aml import *
 from dataclasses import dataclass
-from itertools import chain, combinations, product
+from itertools import chain, combinations, count, product
 
-from typing import Iterable, Iterator, Optional, Union
+from typing import Iterable, Iterator, Optional, TypeVar, Union
 
-# TODO: Make typing more generic.
-def powerset(s: list[TracedPattern]) -> Iterator[Iterable[TracedPattern]]:
+T = TypeVar('T')
+def powerset(s: list[T]) -> Iterator[Iterable[T]]:
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 def definition_list(p: Pattern, existing_list: list[Pattern]) -> list[Pattern]:
@@ -30,15 +30,15 @@ def definition_list(p: Pattern, existing_list: list[Pattern]) -> list[Pattern]:
 @dataclass(frozen=True)
 class TracedPattern:
     """ Annotates patterns with a trace of regenerated definitional constants.  """ 
-    parent: Optional['TracedPattern']
+    trace_id: list[int]
     pattern: Pattern
 
     def left(self) -> TracedPattern:
         assert(isinstance(self.pattern, (App, DApp, And, Or)))
-        return TracedPattern(self.parent, self.pattern.left)
+        return TracedPattern(self.trace_id + [0], self.pattern.left)
     def right(self) -> TracedPattern:
         assert(isinstance(self.pattern, (App, DApp, And, Or)))
-        return TracedPattern(self.parent, self.pattern.right)
+        return TracedPattern(self.trace_id + [1], self.pattern.right)
 
     def closure(self) -> list[list[TracedPattern]]:
         """Calculate possible closures by applying all non-app tableau rules
@@ -50,11 +50,11 @@ class TracedPattern:
         elif isinstance(self.pattern, Not) and isinstance(self.pattern.subpattern, (SVar, EVar, Symbol)):
             return [[self]]
         elif isinstance(self.pattern, And):
-            return TracedPattern.closurePs([TracedPattern(self.parent, self.pattern.left), TracedPattern(self.parent, self.pattern.right)])
+            return TracedPattern.closurePs([self.left(), self.right()])
         elif isinstance(self.pattern, Or):
-            return TracedPattern(self.parent, self.pattern.left).closure() + TracedPattern(self.parent, self.pattern.right).closure()
+            return self.left().closure() + self.right().closure()
         elif isinstance(self.pattern, Mu) or isinstance(self.pattern, Nu):
-            return TracedPattern(self, self.pattern.subpattern.substitute(self.pattern.bound, self.pattern)).closure()
+            return TracedPattern(self.trace_id, self.pattern.subpattern.substitute(self.pattern.bound, self.pattern)).closure()
         else:
             raise NotImplementedError
 
@@ -70,7 +70,7 @@ class TracedPattern:
 class Node:
     @staticmethod
     def make_nodes(patterns: list[Pattern]) -> 'Node':
-        return Node.from_closure([TracedPattern(None, p) for p in patterns])
+        return Node.from_closure([TracedPattern([i], p) for i, p in zip(count(start=0), patterns)])
 
     @staticmethod
     def from_closure(patterns: list[TracedPattern]) -> 'Node':
@@ -90,13 +90,13 @@ class AndNode(Node):
 class Sequent(Node):
     gamma: list[TracedPattern]
 
-    def is_consitant(self) -> bool:
+    def is_inconsistant(self) -> bool:
         gamma : list[Pattern] = [tp.pattern for tp in self.gamma]
         atoms : set[Pattern] = set([phi.negate() for phi in gamma if isinstance(phi, (Symbol, EVar, SVar))])
         return not bool(atoms.intersection(set(gamma)))
 
     def children(self) -> Node:
-        if not self.is_consitant(): return OrNode([])
+        if not self.is_inconsistant(): return OrNode([])
         apps  = [phi for phi in self.gamma if isinstance(phi.pattern, App)]
         dapps = [phi for phi in self.gamma if isinstance(phi.pattern, DApp)]
         left_partitions = powerset(dapps)
@@ -112,11 +112,12 @@ def build_quasimodel(node: Node, path_prefix: list[list[Pattern]]) -> Node:
         gamma_p = [tp.pattern for tp in node.gamma]
         if gamma_p in path_prefix: return node
         else: path_prefix = path_prefix + [gamma_p]
+
         child = node.children()
         if child == AndNode([]): return node
         else: node = child
 
-    if   isinstance(node, AndNode):
+    if isinstance(node, AndNode):
         children = [build_quasimodel(child, path_prefix) for child in node.children]
         if any((child == OrNode([]) for child in children)): return OrNode([])
         return AndNode(children)
@@ -126,3 +127,40 @@ def build_quasimodel(node: Node, path_prefix: list[list[Pattern]]) -> Node:
             if tab != OrNode([]): return tab
         return OrNode([])
     raise NotImplementedError
+
+################################################################################
+
+def is_inconsistant(gamma: list[Pattern]) -> bool:
+    negated_atoms : set[Pattern] = set([phi.negate() for phi in gamma if isinstance(phi, (Symbol, EVar, SVar))])
+    return bool(negated_atoms.intersection(set(gamma)))
+
+def is_satisfiable(gamma: list[Pattern], processed: list[Pattern] = []) -> bool:
+    while gamma:
+        p, *gamma = gamma
+        if   isinstance(p, (Symbol, App, DApp)):
+            processed = processed + [p]
+        elif isinstance(p, Not) and isinstance(p.subpattern, (Symbol)):
+            processed = processed + [p]
+        elif isinstance(p, And):
+            processed = processed + [p.left, p.right]
+#        elif isinstance(p, SVar) and isinstance(p.name, int): # Definitional Constant
+#            return is_satisfiable(gamma, processed + [definition_list[p.name]], definition_list)
+#        elif isinstance(p, Mu) or isinstance(p, Nu):
+#            return is_satisfiable(gamma, processed + [p.subpattern.substitute(p.bound, SVar(definition_list.index(p)))], definition_list)
+        elif isinstance(p, Or):
+            return is_satisfiable(gamma + [p.left],  processed) \
+                or is_satisfiable(gamma + [p.right], processed)
+        else:
+            raise NotImplementedError
+
+    print(processed)
+    print(is_inconsistant(processed))
+
+    if is_inconsistant(processed): return False
+    apps  = [phi for phi in processed if isinstance(phi, App)]
+    dapps = [phi for phi in processed if isinstance(phi, DApp)]
+    left_partitions = powerset(dapps)
+
+    return all( any(     is_satisfiable([app.left]  + list(map(lambda p: p.left,  left)))
+                     and is_satisfiable([app.right] + list(map(lambda p: p.right, set(dapps) - set(left))))
+                    for left in left_partitions) for app in apps )
--- tableau_test.py
+++ tableau_test.py
@@ -2,41 +2,6 @@ from aml import *
 from tableau import *
 from typing import Optional
 
-def tp(p: Pattern, parent: Optional[TracedPattern] = None) -> TracedPattern:
-    return TracedPattern(parent, p)
-
-def test_closure() -> None:
-    def closure(p: Pattern) -> list[list[Pattern]]:
-        return [[traced.pattern for traced in ltp] for ltp in tp(p).closure()]
-
-    assert closure(And(SVar("X"), EVar("Y"))) \
-        == [ [SVar("X"), EVar("Y")] ]
-    assert closure(And(SVar("X"), Or(EVar("Y"), SVar("Z")))) \
-        == [ [SVar("X"), EVar("Y")]
-           , [SVar("X"), SVar("Z")]
-           ]
-    assert closure(Or(SVar("X"), Or(EVar("Y"), SVar("Z")))) \
-        == [ [SVar("X")]
-           , [EVar("Y")]
-           , [SVar("Z")]
-           ]
-    assert closure(And(Or(SVar("X"), Or(EVar("Y"), SVar("Z"))), Symbol("Q"))) \
-        == [ [SVar("X"), Symbol("Q")]
-           , [EVar("Y"), Symbol("Q")]
-           , [SVar("Z"), Symbol("Q")]
-           ]
-    assert closure(Mu(SVar("X"), Or(Symbol("z"), App(Symbol("s"), SVar("X"))))) \
-        == [ [ Symbol("z") ]
-           , [ App(Symbol("s"), Mu(SVar("X"), Or(Symbol("z"), App(Symbol("s"), SVar("X"))))) ]
-           ]
-
-def test_is_consitant() -> None:
-    assert     Sequent([tp(SVar("X"))]).is_consitant()
-    assert     Sequent([tp(SVar("X")), tp(Not(SVar("Y")))]).is_consitant()
-    assert     Sequent([tp(SVar("X")), tp(Not(EVar("X")))]).is_consitant()
-    assert not Sequent([tp(SVar("X")), tp(Not(SVar("X")))]).is_consitant()
-
-
 def test_definition_list() -> None:
     assert definition_list(SVar("X"), []) == []
     assert definition_list(Mu(SVar("X"), App(Symbol("S"), SVar("X"))), []) == [Mu(SVar("X"), App(Symbol("S"), SVar("X")))]
@@ -45,45 +10,37 @@ def test_definition_list() -> None:
            , Nu(SVar("Y"), App(Symbol("S"), SVar(0)))
            ]
 
-def test_children() -> None:
-    assert Sequent([tp(SVar("X")), tp(Not(SVar("X")))]).children() == OrNode([])
-    assert Sequent([tp(SVar("X")), tp(SVar("X"))]).children()      == AndNode([])
+def test_is_satisfiable() -> None:
+    assert     is_satisfiable([Symbol("X")])
+    assert     is_satisfiable([Symbol("X"), Not(Symbol("Y"))])
+    assert not is_satisfiable([Symbol("X"), Not(Symbol("X"))])
 
-    app = tp(App(SVar("X"), SVar("Y")))
-    dapp = tp(DApp(SVar("P"), SVar("Q")))
-    assert Sequent([app, dapp]).children() \
-        == AndNode([ OrNode([ AndNode([ Sequent([tp(SVar("X"))])
-                                      , Sequent([tp(SVar("Y")), tp(SVar("Q"))])
-                                      ])
-                            , AndNode([ Sequent([tp(SVar("X")), tp(SVar("P"))])
-                                      , Sequent([tp(SVar("Y"))])
-                                      ])
-                            ])
-                   ])
+    assert     is_satisfiable([App(Symbol("X"), Symbol("Y"))])
+    assert     is_satisfiable([DApp(Not(Symbol("X")), Not(Symbol("Y")))])
+    assert not is_satisfiable([  App(Symbol("X"), Symbol("Y"))
+                              , DApp(Not(Symbol("X")), Not(Symbol("Y")))
+                              ])
 
-def test_quasimodel() -> None:
-    mu = tp(Mu(SVar("X"), App(Symbol("a"), App(Symbol("b"), SVar("X")))), None)
-    assert build_quasimodel(Node.make_nodes([mu.pattern]), []) \
-        == AndNode([AndNode([ Sequent([tp(Symbol("a"), mu)]),
-           AndNode([AndNode([ Sequent([tp(Symbol("b"), mu)])
-                            , Sequent([tp( App(Symbol("a"), App( Symbol("b") , mu.pattern))
-                                         , tp(mu.pattern, mu)
-                                         )
-                                      ])])])])])
+    assert     is_satisfiable([And(Symbol("X"), Not(Symbol("Y")))])
+    assert not is_satisfiable([And(Symbol("X"), Not(Symbol("X")))])
+    assert not is_satisfiable([And(  App(Symbol("X"), Symbol("Y"))
+                                  , DApp(Not(Symbol("X")), Not(Symbol("Y")))
+                                  )
+                              ])
 
-    assert build_quasimodel(Node.make_nodes([ Or( App( Symbol("S") , And(SVar("X"), Not(SVar("X"))) )
-                                                , App( Symbol("S") , And(SVar("X"), Not(SVar("X"))) )
-                                                )
-                                           ]), []) \
-        == OrNode([])
+    assert     is_satisfiable([And(Symbol("X"), Not(Symbol("Y")))])
+    assert not is_satisfiable([And(Symbol("X"), Not(Symbol("X")))])
+    assert not is_satisfiable([And(  App(Symbol("X"), Symbol("Y"))
+                                  , DApp(Not(Symbol("X")), Not(Symbol("Y")))
+                                  )
+                              ])
 
-    mu = tp(Mu(SVar("X"), And(Symbol("a"), App(Symbol("b"), SVar("X")))))
-    assert build_quasimodel(Node.make_nodes([mu.pattern]), []) \
-        == AndNode([AndNode([ Sequent([ tp(Symbol("b"), mu)])
-                            , Sequent([ tp(Symbol("a"), tp(mu.pattern, mu))
-                                      , tp( App(Symbol("b"), mu.pattern)
-                                          , tp(mu.pattern, mu)
-                                          )
-                                      ]
-                       )])])
-
+    assert     is_satisfiable([Or(Symbol("X"), Not(Symbol("X")))])
+    assert     is_satisfiable([Or(  App(Symbol("X"), Symbol("Y"))
+                                 , DApp(Not(Symbol("X")), Not(Symbol("Y")))
+                                 )
+                              ])
+    assert not is_satisfiable([Or( And(Symbol("X"), Not(Symbol("X")))
+                                 , And(Symbol("X"), Not(Symbol("X")))
+                                 )]
+                             )
