ff9005ee5ca6c829051a5d15cb49ccaefe11d7a2
rod-lin <zl38@illinois.edu>
2021-04-18 20:30:21 -0500

    update image

---
+++ image/init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cat << EOF
+Welcome to the artifact evaluation Docker image for the paper
+
+    Towards a Trustworthy Semantics-Based Language Framework via Proof Generation
+    Xiaohong Chen, Zhengyao Lin, Thai Trinh, and Grigore Rosu
+
+You can find a copy of our original submission at /cav21/paper.pdf
+
+/cav21/README.md contains detailed instruction on how to use this image
+An online version is also available at https://github.com/kframework/matching-logic-proof-checker/blob/cav21-ae/image/README.md
+
+EOF
+
+PS1='\w\$ '
