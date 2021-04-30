a617f04111e7080ecff3952f7d4c17894fd8eb56
John Wang <32222098+jzw2@users.noreply.github.com>
2021-04-29 22:22:47 -0500

    Compression (#23)
    * Sarted writing compress
    
    * Compressed for proofs without mandory
    
    * Added mandatory hypothesis compression
    
    * Added some documentation
    
    * Added type hints
    
    * Changed to use regex
    
    * Switched to using regex
    
    * Added dir and individual file options
    
    * Aded match type

---
+++ scripts/compress.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+from typing import List
+from ml.metamath.parser import load_database
+from ml.metamath.composer import Composer
+
+import os.path
+import argparse
+import re
+import os
+
+
+def number_to_letter(n: int) -> str:
+    number = n - 1
+    final_letter = chr(ord("A") + (number % 20))
+    if number < 20:
+        return final_letter
+
+    number //= 20
+
+    letters = []
+    while True:
+        number -= 1
+        letters.append(chr(ord("U") + ((number % 5))))
+        number //= 5
+        if not number:
+            break
+
+    letters.reverse()
+    letters.append(final_letter)
+    return "".join(letters)
+
+
+def compress(mandatory: List[str], proof: List[str]) -> str:
+    label_to_num = {}
+    unique_proof_labels = list(dict.fromkeys(proof))
+    no_mand = [h for h in unique_proof_labels if h not in mandatory]
+    for i, hyp in enumerate(mandatory + no_mand):
+        label_to_num[hyp] = i + 1
+
+    labels_str = " ( " + " ".join(no_mand) + " ) "
+    letters = [number_to_letter(label_to_num[label]) for label in proof]
+    letters_str = "".join(letters)
+    return labels_str + letters_str
+
+
+parser = argparse.ArgumentParser(description="Compress Metamath proofs")
+parser.add_argument("--inputdir", help="Input directory")
+parser.add_argument("--input", nargs="+", help="Input files")
+parser.add_argument("--outputdir", required=True, help="Output Folder")
+args = parser.parse_args()
+print(args)
+all_inputs = []
+if args.input:
+    all_inputs += args.input
+if args.inputdir:
+    all_inputs += [
+        f.path
+        for f in os.scandir(args.inputdir)
+        if f.is_file() and f.name.endswith(".mm")
+    ]
+for file_name in all_inputs:
+    new_contents = None
+    print("Rewriting " + file_name)
+    with open(file_name, "r") as f:
+        print("loading" + file_name)
+        ast = load_database(file_name, include_proof=False)
+        print("Finished loading ast for " + file_name)
+        composer: Composer = Composer()
+        composer.load(ast)
+
+        contents = f.read()
+
+        def transform(matchgroup: re.Match[str]) -> str:
+            label, statement, proof = matchgroup.groups()
+            theorem = composer.get_theorem(label)
+            mandatory = [x[2] for x in theorem.floatings] + [
+                x.label for x in theorem.essentials if x.label is not None
+            ]
+            new_proof = compress(mandatory, proof.split())
+            return label + " $p " + statement + " $= " + new_proof + " $."
+
+        new_contents = re.sub(r"(\S*)\s*\$p(.*)\$=(.*)\$\.", transform, contents)
+
+    with open(os.path.join(args.outputdir, os.path.split(file_name)[1]), "w") as f2:
+        f2.writelines(new_contents)
