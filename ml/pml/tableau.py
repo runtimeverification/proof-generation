b2ff5c27dc9320634ccbdbd39fd285ff0c730b5b
Nishant Rodrigues <nishantjr@gmail.com>
2021-09-22 13:31:28 -0500

    WIP

--- ml/pml/tableau.py
+++ ml/pml/tableau.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 from abc import abstractmethod
 from dataclasses import dataclass
 from itertools import chain, count, islice, product
-from typing import Any, Dict, FrozenSet, Iterable, Iterator, List, Tuple, TypeVar
+from typing import Any, Dict, FrozenSet, Iterable, Iterator, List, Tuple, TypeVar, Union
 from subprocess import check_output
 import re
 
 from pml import *
+
+Signature = Dict[Symbol, int]
 
 DefList = List[Pattern]
 def definition_list(p: Pattern, def_list: DefList) -> DefList:
@@ -91,17 +93,46 @@ class PGNode():
     assertion: Assertion
     closure: Closure
 
-ParityGame = Dict[PGNode, FrozenSet[PGNode]]
+@dataclass(frozen=True)
+class Root():
+    assertion: Assertion
+
+@dataclass(frozen=True)
+class Sat():
+    pass
+
+@dataclass(frozen=True)
+class Unsat():
+    pass
+
+PGNodeGeneralized = Union[PGNode, Root, Sat, Unsat]
+
+ParityGame = Dict[PGNodeGeneralized, FrozenSet[PGNodeGeneralized]]
 SerializedParityGameEntry = Tuple[int, int, int, List[int]]
 SerializedParityGame = List[SerializedParityGameEntry]
-def print_parity_game(root: PGNode, edges: ParityGame, def_list: DefList) -> SerializedParityGame:
+
+Tableau = Dict[Closure, FrozenSet[Closure]]
+
+def print_parity_game(root: PGNodeGeneralized, edges: ParityGame, def_list: DefList) -> SerializedParityGame:
     ret = []
     keys = list(edges.keys())
 
-    def ident(node: PGNode) -> int:
-        return keys.index(node)
+    def ident(node: PGNodeGeneralized) -> int:
+        if isinstance(node, Root):
+            return 0
+        if isinstance(node, Sat):
+            return 1
+        if isinstance(node, Unsat):
+            return 2
+        return 3 + keys.index(node)
 
-    def priority(node: PGNode, def_list: DefList) -> int:
+    def priority(node: PGNodeGeneralized, def_list: DefList) -> int:
+        if isinstance(node, Root):
+            return 0 # Cannot repeat on any trace, so value doesn't matter.
+        if isinstance(node, Sat):
+            return 0
+        if isinstance(node, Unsat):
+            return 1
         if isinstance(node.assertion, Matches):
             if isinstance(node.assertion.pattern, Nu):
                 return 2 * def_list.index(node.assertion.pattern)
@@ -114,11 +145,14 @@ def print_parity_game(root: PGNode, edges: ParityGame, def_list: DefList) -> Ser
         else:
             return 2 * len(def_list)
 
-    def player(node: PGNode) -> int:
+    def player(node: PGNodeGeneralized) -> int:
+        if isinstance(node, (Root, Sat, Unsat, Top, Bottom, Mu, Nu, SVar, EVar)):
+            # There is no choice to be made here, so it does not matter whose turn it is.
+            return 0 
         if isinstance(node.assertion, Matches):
-            if isinstance(node.assertion.pattern, (Top, And, Forall, DApp, Mu, Nu, SVar, EVar)):
+            if isinstance(node.assertion.pattern, (And, Forall, DApp)):
                 return 1
-            if isinstance(node.assertion.pattern, (Bottom, Or, Exists, App)):
+            if isinstance(node.assertion.pattern, (Or,  Exists, App)):
                 return 0
         if isinstance(node.assertion, AllOf):
             return 1
@@ -129,46 +163,6 @@ def print_parity_game(root: PGNode, edges: ParityGame, def_list: DefList) -> Ser
     for source, destinations in edges.items():
         ret += [(ident(source), priority(source, def_list), player(source), sorted(list(map(ident, destinations))))]
     return ret
-
-def build_parity_game( node         : PGNode
-                     , def_list     : DefList
-                     , edges        : ParityGame
-                     ) -> ParityGame:
-    assertion = node.assertion
-    if isinstance(assertion, Matches):
-        p = assertion.pattern
-        if isinstance(p, (Bottom, Top)):
-            edges[node] = frozenset([node])
-        elif isinstance(p, DApp):
-            if all(map(lambda arg: isinstance(arg, EVar), p.arguments)):
-                if Matches(assertion.variable, p.negate()) not in node.closure:
-                    next_node = PGNode(Matches(assertion.variable, Top()), node.closure)
-                    edges[node] = frozenset([next_node])
-                    build_parity_game(next_node, def_list, edges)
-                else:
-                    next_node = PGNode(Matches(assertion.variable, Bottom()), node.closure)
-                    edges[node] = frozenset([next_node])
-                    build_parity_game(next_node, def_list, edges)
-            else:
-                raise RuntimeError("XXXUnimplemented: " + str(node))
-        elif isinstance(p, App):
-            if all(map(lambda arg: isinstance(arg, EVar), p.arguments)):
-                edges[node] = frozenset([node])
-            else:
-                for tuple in product(p.free_variables().union([assertion.variable]), repeat = len(p.arguments)):
-                    raise RuntimeError("YYYUnimplemented: " + str(node))
-        elif isinstance(p, And):
-            left_node = PGNode(Matches(assertion.variable, p.left), node.closure)
-            right_node = PGNode(Matches(assertion.variable, p.right), node.closure)
-            edges[node] = frozenset([left_node, right_node])
-            build_parity_game(left_node,  def_list, edges)
-            build_parity_game(right_node, def_list, edges)
-
-        else:
-            raise RuntimeError("Unimplemented: " + str(node))
-    else:
-        raise RuntimeError("Unimplemented: " + str(node))
-    return edges
 
 def run_pgsolver(game: SerializedParityGame) -> bool:
     def entry_to_string(entry : SerializedParityGameEntry) -> str:
@@ -183,16 +177,19 @@ def run_pgsolver(game: SerializedParityGame) -> bool:
     return match.group(1) == '0'
 
 # fresh_evar = map(lambda i : EVar('$' + str(i)), count())
-def build_closures(a: Matches, K: List[EVar], signature: Dict[Symbol, int]) -> List[Closure]:
+# 
+def build_closures(a: Matches, K: List[EVar], signature: Signature) -> List[Tuple[Closure, Tuple[Assertion, Assertion]]]:
     closures = add_to_closure(a, frozenset(), K)
     C = a.free_evars().union([a.variable])
     return complete_closure_for_signature(closures, C, K, signature)
 
-def add_app_dapp_to_closure(pairs : List[Tuple[Matches, Matches]] , partialClosure: Closure, K: List[EVar]) -> List[Closure]:
+PartialEdges = List[Tuple[Assertion, Assertion]]
+
+def add_app_dapp_to_closure(pairs : List[Tuple[Matches, Matches]] , partial_closure: Closure, partial_edges: PartialEdges, K: List[EVar]) -> List[Tuple[Closure, PartialEdges]]:
     def is_atomic_application(app : App) -> bool:
         return all(map(lambda arg: isinstance(arg, EVar), app.arguments))
 
-    curr_closures = [partialClosure]
+    curr_closures = [(partial_closure, partial_edges)]
     all_of : List[Assertion] = []
     for (app, dapp) in pairs:
         assert isinstance(app.pattern, App)
@@ -201,58 +198,57 @@ def add_app_dapp_to_closure(pairs : List[Tuple[Matches, Matches]] , partialClosu
 
         if is_atomic_application(app.pattern):
             new_closures = []
-            for curr_closure in curr_closures:
+            for (curr_closure, curr_edges) in curr_closures:
                 if Matches(app.variable, app.pattern.negate()) == dapp:
-                    return add_to_closure(Matches(app.variable, Bottom()), curr_closure, K)
-
+                    return add_to_closure(Matches(app.variable, Bottom()), curr_closure, curr_edges + [(dapp, Matches(app.variable, Bottom()))], K)
 
                 any_of : List[Assertion] = []
                 for ci, phii in zip(app.pattern.arguments, dapp.pattern.arguments):
                     assert isinstance(ci, EVar)
                     any_of += [Matches(ci, phii)]
-                new_closures += add_to_closure(AllOf(frozenset(all_of)), curr_closure, K)
+                new_closures += add_to_closure(AllOf(frozenset(all_of)), curr_closure, curr_edges, K)
             curr_closures = new_closures
     return curr_closures
 
-def add_to_closure(assertion: Assertion, partialClosure: Closure, K: List[EVar]) -> List[Closure]:
+def add_to_closure(assertion: Assertion, partial_closure: Closure, partial_edges: PartialEdges, K: List[EVar]) -> List[Tuple[Closure, Tuple[Assertion, Assertion]]]:
     if isinstance(assertion, Matches):
         p = assertion.pattern
         if   isinstance(p, (Bottom)):
             return []
         elif isinstance(p, (Top)):
-            return [partialClosure.union([assertion])]
+            return [partial_closure.union([assertion])]
         elif isinstance(p, EVar):
             if assertion.variable != p:
-                return add_to_closure(Matches(assertion.variable, Bottom()), partialClosure.union([assertion]), K)
-            return [partialClosure]
+                return add_to_closure(Matches(assertion.variable, Bottom()), partial_closure.union([assertion]), K)
+            return [partial_closure]
         elif isinstance(p, Not):
             assert isinstance(p.subpattern, EVar)
-            if Matches(assertion.variable, p.negate()) in partialClosure:
-                return add_to_closure(Matches(assertion.variable, Bottom()), partialClosure.union([assertion]), K)
-            return [partialClosure]
+            if Matches(assertion.variable, p.negate()) in partial_closure:
+                return add_to_closure(Matches(assertion.variable, Bottom()), partial_closure.union([assertion]), K)
+            return [partial_closure]
         elif isinstance(p, DApp):
-            apps = [a for a in partialClosure if    isinstance(a, Matches)
+            apps = [a for a in partial_closure if    isinstance(a, Matches)
                                                 and isinstance(a.pattern, App)
                                                 and a.pattern.symbol == p.symbol]
-            return add_app_dapp_to_closure(list(product(apps, [assertion])), partialClosure.union(frozenset([assertion])), K)
+            return add_app_dapp_to_closure(list(product(apps, [assertion])), partial_closure.union(frozenset([assertion])), K)
         elif isinstance(p, App):
-            dapps = [a for a in partialClosure if   isinstance(a, Matches)
+            dapps = [a for a in partial_closure if   isinstance(a, Matches)
                                                 and isinstance(a.pattern, DApp)
                                                 and a.pattern.symbol == p.symbol]
-            return add_app_dapp_to_closure(list(product([assertion], dapps)), partialClosure.union(frozenset([assertion])), K)
+            return add_app_dapp_to_closure(list(product([assertion], dapps)), partial_closure.union(frozenset([assertion])), K)
         elif isinstance(p, And):
             ret = []
-            partialClosure = partialClosure.union([assertion])
-            for closure in add_to_closure(Matches(assertion.variable, p.left),  partialClosure, K):
+            partial_closure = partial_closure.union([assertion])
+            for closure in add_to_closure(Matches(assertion.variable, p.left),  partial_closure, K):
                 ret += add_to_closure(Matches(assertion.variable, p.right), closure, K)
             return ret
         else:
             raise RuntimeError("Unimplemented: ")
     elif isinstance(assertion, AllOf):
         if len(assertion.assertions) == 0:
-            return [partialClosure]
+            return [partial_closure]
         first, *rest = assertion.assertions
-        new_closures = add_to_closure(first, partialClosure, K)
+        new_closures = add_to_closure(first, partial_closure, K)
         ret = []
         for new_closure in new_closures:
             ret += add_to_closure(AllOf(frozenset(rest)), new_closure, K)
@@ -262,12 +258,12 @@ def add_to_closure(assertion: Assertion, partialClosure: Closure, K: List[EVar])
             return []
         ret = []
         for a in assertion.assertions:
-            ret += add_to_closure(a, partialClosure, K)
+            ret += add_to_closure(a, partial_closure, K)
         return ret
     else:
         raise RuntimeError("Unimplemented: ")
 
-def complete_closure_for_signature(closures: List[Closure], C: FrozenSet[EVar], K: List[EVar], signature: Dict[Symbol, int]) -> List[Closure]:
+def complete_closure_for_signature(closures: List[Closure], C: FrozenSet[EVar], K: List[EVar], signature: Signature) -> List[Closure]:
     # TODO: This should be replaced by some form of resulution
     for (symbol, arity) in signature.items():
         for tuple in product(C, repeat = arity + 1):
@@ -281,8 +277,6 @@ def complete_closure_for_signature(closures: List[Closure], C: FrozenSet[EVar], 
             closures = new_closures
     return closures
 
-Tableau = Dict[Closure, FrozenSet[Closure]]
-
 def diff(l1: List[EVar] , l2_set: FrozenSet[EVar]) -> List[EVar]:
     # Preserves order
     return [item for item in l1 if item not in l2_set]
@@ -290,8 +284,8 @@ def diff(l1: List[EVar] , l2_set: FrozenSet[EVar]) -> List[EVar]:
 def take(n: int, l: List[EVar]) -> List[EVar]:
   return list(islice(l, n))
 
-def build_tableau(currentNode: Closure, partialTableau: Tableau, K: List[EVar], signature: Dict[Symbol, int]) -> Tableau:
-    if currentNode in partialTableau.keys(): return partialTableau
+def build_tableau(currentNode: Closure, partialTableau: Tableau, partialGame : ParityGame, K: List[EVar], signature: Signature) -> None:
+    if currentNode in partialTableau.keys(): return
     next_nodes : List[Closure] = []
     for assertion in currentNode:
         if not isinstance(assertion, Matches):
@@ -320,21 +314,29 @@ def build_tableau(currentNode: Closure, partialTableau: Tableau, K: List[EVar], 
     partialTableau[currentNode] = frozenset(next_nodes)
 
     for node in next_nodes:
-        build_tableau(node, partialTableau, K, signature)
-    return partialTableau
+        build_tableau(node, partialTableau, partialGame, K, signature)
 
-def build_tableaux(assertion : Matches, K: List[EVar], signature: Dict[Symbol, int]) -> List[Tableau]:
+def build_tableaux(assertion : Matches, K: List[EVar], signature: Signature) -> ParityGame:
     def_list : DefList = definition_list(assertion.pattern, def_list = [])
-    ret : List[Tableau] = []
-    for closure in build_closures(assertion, K, signature):
-        root = closure
-        ret += [build_tableau(root, {}, K, signature)]
-    return ret
+    game : ParityGame = {}
+    tableau : Tableau = {}
+    closures = build_closures(assertion, K, signature, game)
+    if closures:
+        game[Root(assertion)] = frozenset([PGNode(assertion, closure) for closure in closures])
+    else:
+        game[Root(assertion)] = frozenset([Unsat()])
+    for closure in closures:
+        build_tableau(closure, tableau, game, K, signature)
+    return game
 
 def tableau_to_parity_game(tableau: Tableau) -> ParityGame:
     pass
 
-def is_sat(p: Pattern) -> bool:
+def is_sat(p: Pattern, K: List[EVar], signature: Signature) -> bool:
+    tableaux = build_tableaux(Matches(K[0], p), K, signature)
+    for tableau in tableaux:
+        print(tableau)
+        return True
     return False
 #def is_sat(p: Pattern) -> bool:
 #    assertion = Matches(fresh_evar.__next__(), p)
--- ml/pml/tableau_test.py
+++ ml/pml/tableau_test.py
@@ -1,6 +1,6 @@
 from pml import *
 from tableau import *
-from typing import Dict, List
+from typing import Dict, List, cast
 
 S = Symbol("S")
 C = Symbol("C")
@@ -44,29 +44,40 @@ def test_build_tableaux() -> None:
     signature : Dict[Symbol, int] = {} 
     closures = set(build_closures(assertion, constants, signature))
     assert closures == set()
-    tableaux = build_tableaux(assertion, constants, signature)
-    assert tableaux == []
+    game = build_tableaux(assertion, constants, signature)
+    root : PGNodeGeneralized = Root(assertion)
+    assert game == {    root : frozenset([cast(PGNodeGeneralized, Unsat())])
+                   }
 
     assertion = Matches(c, Top())
     signature = {}
     closures = set(build_closures(assertion, constants, signature))
     assert closures == {frozenset([assertion])}
-    tableaux = build_tableaux(assertion, constants, signature)
-    assert tableaux == [ { frozenset([assertion]) : frozenset() } ]
+    game = build_tableaux(assertion, constants, signature)
+    print(game)
+    assert game == {                           Root(assertion) : frozenset([PGNode(assertion, frozenset([assertion]))])
+                   , PGNode(assertion, frozenset([assertion])) : frozenset([PGNode(assertion, frozenset([assertion]))])
+                   }
 
     assertion = Matches(c, App(C))
     signature = { C : 0 }
     closures = set(build_closures(assertion, constants, signature))
     assert closures == {frozenset([assertion])}
-    tableaux = build_tableaux(assertion, constants, signature)
-    assert tableaux == [ { frozenset([assertion]) : frozenset() } ]
+    game = build_tableaux(assertion, constants, signature)
+    assert game == {                           Root(assertion) : frozenset([PGNode(assertion, frozenset([assertion]))])
+                   , PGNode(assertion, frozenset([assertion])) : frozenset([PGNode(assertion, frozenset([assertion]))])
+                   }
 
     assertion = Matches(c, DApp(C))
     signature = { C : 0 }
     closures = set(build_closures(assertion, constants, signature))
     assert closures == {frozenset([assertion])}
-    tableaux = build_tableaux(assertion, constants, signature)
-    assert tableaux == [ { frozenset([assertion]) : frozenset() } ]
+    game = build_tableaux(assertion, constants, signature)
+    assert game == {                           Root(assertion) : frozenset([PGNode(assertion, frozenset([assertion]))])
+                   , PGNode(assertion, frozenset([assertion])) : frozenset([PGNode(assertion, frozenset([assertion]))])
+                   }
+    return
+
     assertion = Matches(c, App(S, App(C)))
     signature = { S : 1, C : 0 }
 
@@ -89,7 +100,6 @@ def test_build_tableaux() -> None:
 
     closures = set(build_closures(assertion, constants, signature))
     assert closures == { cl_00___, cl_10___, cl_01___, cl_11___ }
-    tableaux = build_tableaux(assertion, [c, c1], signature)
 
     cl_next___0000 = frozenset([m___0___, m____0__, m_____0_, m______0])
     cl_next___0010 = frozenset([m___0___, m____0__, m_____1_, m______0])
@@ -100,6 +110,11 @@ def test_build_tableaux() -> None:
     cl_next___0101 = frozenset([m___0___, m____1__, m_____0_, m______1])
     cl_next___0111 = frozenset([m___0___, m____1__, m_____1_, m______1])
     cl_next = [ cl_next___0000, cl_next___0010, cl_next___0100, cl_next___0110 ]
+
+    game = build_tableaux(assertion, [c, c1], signature)
+
+    assert game[Root(assertion)]             == frozenset([cl_00___, cl_01___, cl_10___, cl_11___])
+#    assert game[PGNode(assertion, cl_00__))] == frozenset([])
 
     assert tableaux[0] == { cl_00___ :  frozenset([cl_00___]) }
     assert cl_01___.union(cl_next___0000) in tableaux[1][cl_01___]
@@ -160,17 +175,19 @@ def test_build_tableaux() -> None:
     assert { cl_11 : frozenset() } in tableaux
 
 def xtest_is_satisfiable() -> None:
-    assert not is_sat(Bottom())
-    assert     is_sat(Top())
+    signature = {C: 0, S: 1}
 
-    assert not is_sat(And(EVar('c'), Not(EVar('c'))))
-    assert not is_sat(And(Not(EVar('c')), EVar('c')))
+    assert not is_sat(Bottom(), [c], signature)
+    assert     is_sat(Top(),    [c], signature)
 
-    assert     is_sat(App(S))
-    assert     is_sat(DApp(S))
-    assert not is_sat(And(App(S),DApp(S)))
-
-    assert     is_sat(App(S, App(C)))
+    assert not is_sat(And(EVar('c'), Not(EVar('c'))), [c], signature)
+    assert not is_sat(And(Not(EVar('c')), EVar('c')), [c], signature)
+#
+#    assert     is_sat(App(S))
+#    assert     is_sat(DApp(S))
+#    assert not is_sat(And(App(S),DApp(S)))
+#
+#    assert     is_sat(App(S, App(C)))
 
 #    assert not is_sat(And(App(S, App(C)), DApp(S, DApp(C))))
 
@@ -218,30 +235,30 @@ def xtest_is_satisfiable() -> None:
 #                                        , App(NEXT , X)
 #                                        )))
 
-def test_print_parity_game() -> None:
-    p0  = Matches(e, Mu(X, App(S, x)))
-    p1  = Matches(e, App(S, App(C), x))
-    p21 = Matches(e, App(S, e1, x))
-    p22 = Matches(e1, App(C))
-    p2  = AllOf(frozenset([ p21 , p22 ]))
-    closure = frozenset( [ p0
-                         , p1
-                         , p2
-                         , p21
-                         , p22
-                         ] )
-    assert print_parity_game( PGNode(p0, closure)
-                            , { PGNode(p0, closure)  : frozenset([ PGNode(p1, closure) ])
-                              , PGNode(p1, closure)  : frozenset([ PGNode(p2, closure) ])
-                              , PGNode(p2, closure)  : frozenset([ PGNode(p21, closure), PGNode(p22, closure) ])
-                              , PGNode(p21, closure) : frozenset([])
-                              , PGNode(p22, closure) : frozenset([])
-                              }
-                            , definition_list(Mu(X, App(S, x)), [])
-                            ) == \
-                            [ (0, 1, 1, [1])
-                            , (1, 2, 0, [2])
-                            , (2, 2, 1, [3, 4])
-                            , (3, 2, 0, [])
-                            , (4, 2, 0, [])
-                            ]
+#def test_print_parity_game() -> None:
+#    p0  = Matches(e, Mu(X, App(S, x)))
+#    p1  = Matches(e, App(S, App(C), x))
+#    p21 = Matches(e, App(S, e1, x))
+#    p22 = Matches(e1, App(C))
+#    p2  = AllOf(frozenset([ p21 , p22 ]))
+#    closure = frozenset( [ p0
+#                         , p1
+#                         , p2
+#                         , p21
+#                         , p22
+#                         ] )
+#    assert print_parity_game( PGNode(p0, closure)
+#                            , { PGNode(p0, closure)  : frozenset([ PGNode(p1, closure) ])
+#                              , PGNode(p1, closure)  : frozenset([ PGNode(p2, closure) ])
+#                              , PGNode(p2, closure)  : frozenset([ PGNode(p21, closure), PGNode(p22, closure) ])
+#                              , PGNode(p21, closure) : frozenset([])
+#                              , PGNode(p22, closure) : frozenset([])
+#                              }
+#                            , definition_list(Mu(X, App(S, x)), [])
+#                            ) == \
+#                            [ (0, 1, 1, [1])
+#                            , (1, 2, 0, [2])
+#                            , (2, 2, 1, [3, 4])
+#                            , (3, 2, 0, [])
+#                            , (4, 2, 0, [])
+#                            ]
