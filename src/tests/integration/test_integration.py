e4e1abf11115b3a0058d30d82054093fbca2b6ef
Tamás Tóth <tothtamas28@users.noreply.github.com>
2023-05-26 11:39:50 +0200

    Port integration test to `pytest`

--- src/tests/integration/test_integration.py
+++ src/tests/integration/test_integration.py
@@ -1,92 +1,60 @@
-import os
-import shutil
-import tempfile
-import unittest
+from pathlib import Path
+
+import pytest
 
 from proof_generation.ml.rewrite.__main__ import main as rewrite_main
 
+REPO_ROOT = (Path(__file__).parents[3]).resolve(strict=True)
+TEST_DATA_DIR = (Path(__file__).parent / 'test-data').resolve(strict=True)
 
-class TestIntegrationBase(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.output_proof_dir = tempfile.mkdtemp()
-
-    def tearDown(self) -> None:
-        shutil.rmtree(self.output_proof_dir)
-
-    def run_rewrite_proof_gen(self, pgm: str) -> None:
-        """
-        pgm should point to a directory under the `integration` directory
-        the suffix of the name of pgm should be of the form ".<defn>"
-        where defn should have a corresponding .kore file under the `definitions` directory
-        
-        e.g. integration/pgm-1.foo -> definitions/foo.kore
-
-        The main module in any definition should be the capitalization of the name of the definition
-        e.g. foo.kore should have FOO as its main module
-        typed-lambda.kore should have TYPED_LAMBDA as its main module
-        """
-
-        assert os.path.isdir(self.output_proof_dir)
-
-        definitions_path = os.path.join('tests', 'definitions')
-        integration_path = os.path.join('tests', 'integration')
-        prelude_path = os.path.join('theory', 'prelude.mm')
-
-        assert os.path.isdir(definitions_path) and \
-               os.path.isdir(integration_path) and \
-               os.path.isfile(prelude_path), \
-               'not in the right directory'
-
-        task_path = os.path.join(integration_path, f'{pgm}.yml')
-        assert os.path.isfile(task_path), \
-               f'cannot find task file {task_path}'
-
-        definition_name = pgm.split('.')[-1]
-        definition_path = os.path.join(definitions_path, f'{definition_name}.kore')
-
-        assert os.path.isfile(definition_path), \
-               f'cannot find definition {definition_path} inferred from program name {pgm}'
-
-        module_name = definition_name.upper()
-
-        rewrite_main(
-            [
-                definition_path,
-                module_name,
-                '--prelude',
-                prelude_path,
-                '--task',
-                task_path,
-                # '--output',
-                # self.output_proof_dir,
-            ]
-        )
+PRELUDE_FILE = (REPO_ROOT / 'theory' / 'prelude.mm').resolve(strict=True)
+DEFINITIONS_DIR = (TEST_DATA_DIR / 'definitions').resolve(strict=True)
+INTEGRATION_DIR = (TEST_DATA_DIR / 'integration').resolve(strict=True)
 
 
-class TestRewriteProofGeneratorIntegeration(TestIntegrationBase):
-    # TODO: verify using metamath
-    def test_foo(self) -> None:
-        self.run_rewrite_proof_gen('pgm-1.foo')
+"""
+Each pgm for testing should be place in the `integration` directory
+the suffix of the name of pgm should be of the form ".<defn>"
+where defn should have a corresponding .kore file under the `definitions` directory
 
-    def test_lambda(self) -> None:
-        self.run_rewrite_proof_gen('pgm-1.lambda-custom-substitution')
+e.g. integration/pgm-1.foo -> definitions/foo.kore
 
-    def test_imp(self) -> None:
-        self.run_rewrite_proof_gen('pgm-1.imp-custom-map')
+The main module in any definition should be the capitalization of the name of the definition
+e.g. foo.kore should have FOO as its main module
+typed-lambda.kore should have TYPED_LAMBDA as its main module
+"""
+TEST_FILES = tuple(INTEGRATION_DIR.glob('*.yml'))
 
-    def test_map_test_comm(self) -> None:
-        self.run_rewrite_proof_gen('pgm-comm.map-test')
 
-    def test_map_test_ac(self) -> None:
-        self.run_rewrite_proof_gen('pgm-ac-hard.map-test')
+def defn_for(pgm_file: Path) -> Path:
+    test_name = pgm_file.name
+    pgm_name, defn_name, yml = test_name.split('.')
+    assert yml == 'yml'
+    defn_file = (DEFINITIONS_DIR / f'{defn_name}.kore').resolve(strict=True)
+    return defn_file
 
-    def test_map_test_unit(self) -> None:
-        self.run_rewrite_proof_gen('pgm-unit.map-test')
 
-    def test_map_test_acu(self) -> None:
-        self.run_rewrite_proof_gen('pgm-acu-hard.map-test')
+TEST_DATA = tuple((pgm_file, defn_for(pgm_file)) for pgm_file in TEST_FILES)
 
-    def test_owise(self) -> None:
-        # a function with a owise rule and 3 other rules
-        self.run_rewrite_proof_gen('pgm.owise')
+
+@pytest.mark.parametrize('pgm_file,defn_file', TEST_DATA, ids=[pgm_file.name for pgm_file, _ in TEST_DATA])
+def test_rewrite_proof_generator(pgm_file: Path, defn_file: Path, tmp_path: Path) -> None:
+    # Given
+    module_name = defn_file.stem.upper()
+
+    # When
+    rewrite_main(
+        [
+            str(defn_file),
+            module_name,
+            '--prelude',
+            str(PRELUDE_FILE),
+            '--task',
+            str(pgm_file),
+            '--output',
+            str(tmp_path),
+        ]
+    )
+
+    # Then
+    assert True
