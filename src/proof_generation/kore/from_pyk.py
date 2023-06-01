82c68fce7a32cd40ef6b7866db600dc1bd69beee
Nishant Rodrigues <nishantjr@gmail.com>
2023-06-01 14:23:19 -0500

    proof_generation.kore.from_pyk: Allow translation of configuration patterns from pyk to pg.kore

---
+++ src/proof_generation/kore/from_pyk.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from typing import overload
+
+import pyk.kore.syntax as pyk_kore
+
+import proof_generation.kore.ast as pg_kore
+
+
+@overload
+def from_pyk(p: pyk_kore.SortVar) -> pg_kore.SortVariable:
+    ...
+
+
+@overload
+def from_pyk(p: pyk_kore.SortApp) -> pg_kore.SortInstance:
+    ...
+
+
+@overload
+def from_pyk(p: pyk_kore.Sort) -> pg_kore.Sort:
+    ...
+
+
+@overload
+def from_pyk(p: pyk_kore.Pattern) -> pg_kore.Pattern:
+    ...
+
+
+def from_pyk(p: pyk_kore.Pattern | pyk_kore.Sort) -> pg_kore.Pattern | pg_kore.Sort:
+    if isinstance(p, pyk_kore.SortApp):
+        sorts: list[pg_kore.Sort] = [from_pyk(sort) for sort in p.sorts]
+        return pg_kore.SortInstance(p.name, sorts)
+    if isinstance(p, pyk_kore.EVar):
+        sort = from_pyk(p.sort)
+        return pg_kore.Variable(p.name, sort)
+    if isinstance(p, pyk_kore.DV):
+        sorts = [from_pyk(p.sort)]
+        args: list[pg_kore.Pattern] = [pg_kore.StringLiteral(p.value.value)]
+        return pg_kore.MLPattern('\\dv', sorts, args)
+    if isinstance(p, pyk_kore.SortVar):
+        return pg_kore.SortVariable(p.name)
+    if isinstance(p, pyk_kore.App):
+        sorts = [from_pyk(sort) for sort in p.sorts]
+        args = [from_pyk(arg) for arg in p.args]
+        return pg_kore.Application(pg_kore.SymbolInstance(p.symbol, sorts), args)
+    raise NotImplementedError('For type {}, kore = {}'.format(type(p), p))
