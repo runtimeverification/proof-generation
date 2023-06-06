2cd1cfe6af0f5b41be99455d420713c743eacf11
Adam Fiedler <info@fiedlr.sk>
2023-06-06 14:49:11 +0200

    Add brackets to avoid the need for precedence and remove subtraction in the comparison

--- examples/machine-learning/svm.py
+++ examples/machine-learning/svm.py
@@ -5,10 +5,10 @@ def svm(X, W, b):
     s = 0
 
     while i < len(X):
-        s = s + W[i] * X[i]
+        s = s + (W[i] * X[i])
         i = i + 1
 
-    if s - b > 0:
+    if s > b:
       return 1
 
     return -1
